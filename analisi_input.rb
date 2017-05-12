2c6d0e1ef514f5919cc352101b8afbcceb49d049
Alessio Fanelli <fanahova@gmail.com>
2017-05-13 01:20:34 +0200

    Aggiungi risposte (#4)
    * Add answers
    
    * Muovi codice

---
+++ analisi_input.rb
@@ -0,0 +1,39 @@
+require_relative './lib/argomenti'
+require_relative './lib/tutor'
+require_relative './lib/risposte'
+require_relative './lib/messaggi'
+
+class AnalisiInput
+  def self.domande(text)
+    data = text.match(array_to_regexp(Argomenti.tutti, true))
+    unless data.nil?
+      "#{Risposte::Testuale.new(data[1]).crea_descrizione}"
+    else
+      "Non abbiamo trovato nulla su #{data}, mi dispiace! :("
+    end
+  end
+
+  def self.tutorial(text)
+    data = text.match(/tutorial \b\w*\b (.*)/)
+    if data
+      link = Risposte::Tutorial.new(data[1]).trova_link
+      "Ti abbiamo trovato un tutorial! Vai su #{link}"
+    else
+      "Nessun tutorial trovato su #{data} :("
+    end
+  end
+
+  def self.tutor(text)
+    data = text.match(array_to_regexp(Tutor::DISPONIBILI, true))
+    if data
+      Tutor.per(data[1])
+    else
+      "Non abbiamo trovato tutor per il tuo argomento :("
+    end
+  end
+end
+
+def array_to_regexp(array, catch_data = false)
+  return Regexp.new(array.join('|')) unless catch_data
+  Regexp.new("(#{array.join('|')})")
+end
