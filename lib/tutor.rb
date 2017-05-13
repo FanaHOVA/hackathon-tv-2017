fa2ead0e8d474b1ab5bca0885e398b8c9fb83843
Alessio Fanelli <fanahova@gmail.com>
2017-05-13 02:38:13 +0200

    Add node

--- lib/tutor.rb
+++ lib/tutor.rb
@@ -1,7 +1,8 @@
 class Tutor
   DISPONIBILI = ['html', 'css', 'c', 'node']
   TUTORS = { 'html' => ['Alessio', 'Manuel'],
-             'css' => ['Andrea'] }
+             'css' => ['Andrea', 'Federico'],
+             'node' => ['Andrea', 'Alessio'] }
 
   def self.per(argomento)
     "Ci sono #{TUTORS[argomento].length} tutor per #{argomento}: #{TUTORS[argomento].join(', ')}"
