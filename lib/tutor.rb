75ad397b34536a82760245f4e4ee43f9031ade08
Alessio Fanelli <fanahova@gmail.com>
2017-05-13 05:11:10 +0200

    Aggiungi Tutor C

--- lib/tutor.rb
+++ lib/tutor.rb
@@ -2,7 +2,8 @@ class Tutor
   DISPONIBILI = ['html', 'css', 'c', 'node']
   TUTORS = { 'html' => ['Alessio', 'Manuel'],
              'css' => ['Andrea', 'Federico'],
-             'node' => ['Andrea', 'Alessio'] }
+             'node' => ['Andrea', 'Alessio'],
+             'c' => ['Andrea', 'Alessio'] }
 
   def self.per(argomento)
     "Ci sono #{TUTORS[argomento].length} tutor per #{argomento}: #{TUTORS[argomento].join(', ')}"
